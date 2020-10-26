c655c1778b33ae07090a5c42188802b1ee2e6ff5
hrittwik <hrittwik@gmail.com>
2020-10-26 13:00:40 +0600

    data comes for one city from openweatherapi ui still needs work

--- the_weather/weather/views.py
+++ the_weather/weather/views.py
@@ -1,5 +1,21 @@
+import requests
 from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'weather/weather.html')
+    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&APPID=bf3d17ed87ffd351bd143812ed51814e'
+    city = 'Dhaka'
+    
+    r = requests.get(url.format(city)).json()
+
+    city_weather = {
+        'city' : city,
+        'temperature' : r['main']['temp'],
+        'description' : r['weather'][0]['description'],
+        'icon' : r['weather'][0]['icon'],
+    }
+
+    print(city_weather)
+    context = {'city_weather' : city_weather}
+
+    return render(request, 'weather/weather.html', context)
